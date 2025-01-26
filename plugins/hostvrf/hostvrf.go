YutaroHayakawa/hostvrf-cni

commit 42934a0d7d5cedd64a3bba0266fd4f1bb07fc324

Don't assign loopback address to the VRF
Assigning the loopback address to the VRF is not a strict
requirement. While it is still useful to realize the container -> host
BGP peering, we don't need to consider it at this point.

Signed-off-by: Yutaro Hayakawa <yhayakawa3720@gmail.com>

plugins/hostvrf/hostvrf.go
@@ -220,33 +220,6 @@ func ensureVRF(name string, requestedTable uint32) (*netlink.Vrf, error) {
 	return vrf, nil
 }
 
-// ensureLoopbackAddresses assigns loopback addresses to the VRF device
-func ensureLoopbackAddresses(n *NetConf, vrf *netlink.Vrf) error {
-	if n.hasV4() {
-		if err := netlink.AddrReplace(vrf, &netlink.Addr{
-			IPNet: &net.IPNet{
-				IP:   n.loopbackAddressV4,
-				Mask: net.CIDRMask(32, 32),
-			},
-		}); err != nil {
-			return err
-		}
-	}
-
-	if n.hasV6() {
-		if err := netlink.AddrReplace(vrf, &netlink.Addr{
-			IPNet: &net.IPNet{
-				IP:   n.loopbackAddressV6,
-				Mask: net.CIDRMask(128, 128),
-			},
-		}); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
 // ensureUnreachableDefaultRoutes inserts unreachable routes to the VRF device.
 // This route is used to isolate the traffic from the VRF device to the outside
 // world by default. In Linux, VRF is implemented as an IP rule which is
@@ -303,11 +276,6 @@ func setupVRF(n *NetConf) (*netlink.Vrf, *current.Interface, error) {
 	vrf, err := ensureVRF(n.VRFName, n.VRFTable)
 	if err != nil {
 		return nil, nil, err
-	}
-
-	// assign loopback addresses to the VRF
-	if err := ensureLoopbackAddresses(n, vrf); err != nil {
-		return nil, nil, fmt.Errorf("failed to assign loopback addresses to VRF: %w", err)
 	}
 
 	// insert unreachable default route for the isolation

plugins/hostvrf/hostvrf_test.go
@@ -527,31 +527,6 @@ func TestCNIAddDel(t *testing.T) {
 					require.NoError(t, err)
 				})
 
-				t.Run("VRF loopback IPs are assigned", func(t *testing.T) {
-					err = c.ExecFunc(ctx, func(_ ns.NetNS) error {
-						if netConf.LoopbackAddressV4 != "" {
-							addrs, err := netlink.AddrList(vrf, netlink.FAMILY_V4)
-							require.NoError(t, err)
-							require.Len(t, addrs, 1, "IPv4 loopback address is missing")
-							expected := net.ParseIP(netConf.LoopbackAddressV4)
-							require.NotNil(t, expected)
-							require.Equal(t, expected.String(), addrs[0].IP.String(),
-								"Unexpected IPv4 loopback address %q is assigned to the VRF", addrs[0].IP.String())
-						}
-						if netConf.LoopbackAddressV6 != "" {
-							addrs, err := netlink.AddrList(vrf, netlink.FAMILY_V6)
-							require.NoError(t, err)
-							require.Len(t, addrs, 1, "IPv6 loopback address is missing")
-							expected := net.ParseIP(netConf.LoopbackAddressV6)
-							require.NotNil(t, expected)
-							require.Equal(t, expected.String(), addrs[0].IP.String(),
-								"Unexpected IPv6 loopback address %q is assigned to the VRF", addrs[0].IP.String())
-						}
-						return nil
-					})
-					require.NoError(t, err)
-				})
-
 				var hostVeth *netlink.Veth
 
 				t.Run("Host-side veth is enslaved to the VRF", func(t *testing.T) {
@@ -831,9 +806,8 @@ func TestCNIAddDel(t *testing.T) {
 								},
 							)
 							require.NoError(t, err)
-							require.Len(t, routes, 2, "Unexpected number of IPv4 routes are left")
+							require.Len(t, routes, 1, "Unexpected number of IPv4 routes are left")
 							require.Contains(t, routes, "0.0.0.0/0", "Unreachable default route is missing after DEL")
-							require.Contains(t, routes, netConf.LoopbackAddressV4+"/32", "Loopback route is missing after DEL")
 						}
 						if netConf.LoopbackAddressV6 != "" {
 							routes := map[string]netlink.Route{}
@@ -849,9 +823,8 @@ func TestCNIAddDel(t *testing.T) {
 								},
 							)
 							require.NoError(t, err)
-							require.Len(t, routes, 2, "Unexpected number of IPv6 routes are left")
+							require.Len(t, routes, 1, "Unexpected number of IPv6 routes are left")
 							require.Contains(t, routes, "::/0", "Unreachable default route is missing after DEL")
-							require.Contains(t, routes, netConf.LoopbackAddressV6+"/128", "Loopback route is missing after DEL")
 						}
 						return nil
 					})

commit 27dd468da7ff02d1f12ce9edfd8be50ee4fa1395

Improve container->host connetivity mechanism
This commit introduces some improvements for container->host
connectivity mechanism.

1. Assign IPv4 dummy gateway address to the host-side veth. This makes
   gateway adress always reachable in the VRF and fixes the broken
   proxy_arp introduced in 42934a0d.
2. Use link local address as a dummy gateway address for IPv6. This
   makes dummyGatewayAddressV6 configuration unnecessary.

Signed-off-by: Yutaro Hayakawa <yhayakawa3720@gmail.com>

plugins/hostvrf/hostvrf.go
@@ -43,16 +43,13 @@ type NetConf struct {
 	EnableIPv4            bool   `json:"enableIPv4"`
 	EnableIPv6            bool   `json:"enableIPv6"`
 	DummyGatewayAddressV4 string `json:"dummyGatewayAddressV4"`
-	DummyGatewayAddressV6 string `json:"dummyGatewayAddressV6"`
 
 	// Private fields used internally. Filled at the load time.
 	dummyGatewayAddressV4 net.IP `json:"-"`
-	dummyGatewayAddressV6 net.IP `json:"-"`
 }
 
 const (
 	defaultDummyGatewayAddressV4 = "169.254.0.1"
-	defaultDummyGatewayAddressV6 = "fd00:169:254::1"
 )
 
 func init() {
@@ -84,10 +81,6 @@ func loadNetConf(data []byte) (*NetConf, string, error) {
 		n.DummyGatewayAddressV4 = defaultDummyGatewayAddressV4
 	}
 
-	if n.DummyGatewayAddressV6 == "" {
-		n.DummyGatewayAddressV6 = defaultDummyGatewayAddressV6
-	}
-
 	dummyGatewayAddressV4 := net.ParseIP(n.DummyGatewayAddressV4)
 	if dummyGatewayAddressV4 == nil {
 		return nil, "", fmt.Errorf("failed to parse IPv4 dummy gateway address %q", n.DummyGatewayAddressV4)
@@ -96,15 +89,6 @@ func loadNetConf(data []byte) (*NetConf, string, error) {
 		return nil, "", fmt.Errorf("dummyGatewayAddressV4 must be an IPv4 address")
 	}
 	n.dummyGatewayAddressV4 = dummyGatewayAddressV4
-
-	dummyGatewayAddressV6 := net.ParseIP(n.DummyGatewayAddressV6)
-	if dummyGatewayAddressV6 == nil {
-		return nil, "", fmt.Errorf("failed to parse IPv6 dummy gateway address %q", n.DummyGatewayAddressV6)
-	}
-	if dummyGatewayAddressV6.To4() != nil {
-		return nil, "", fmt.Errorf("dummyGatewayAddressV6 must be an IPv6 address")
-	}
-	n.dummyGatewayAddressV6 = dummyGatewayAddressV6
 
 	return n, n.CNIVersion, nil
 }
@@ -288,7 +272,77 @@ func setupVRF(n *NetConf) (*netlink.Vrf, *current.Interface, error) {
 	}, nil
 }
 
-func setupVeth(netns ns.NetNS, vrf *netlink.Vrf, ifName string) (*current.Interface, *current.Interface, error) {
+func setHostVethSysctls(n *NetConf, hostVethLink netlink.Link) error {
+	if n.EnableIPv4 {
+		// Setup proxy-arp to the host interface
+		if _, err := sysctl.Sysctl(fmt.Sprintf("net/ipv4/conf/%s/proxy_arp", hostVethLink.Attrs().Name), "1"); err != nil {
+			return fmt.Errorf("failed to set proxy_arp: %w", err)
+		}
+
+		// Enable IPv4 forwarding
+		if _, err := sysctl.Sysctl(fmt.Sprintf("net/ipv4/conf/%s/forwarding", hostVethLink.Attrs().Name), "1"); err != nil {
+			return fmt.Errorf("failed to set forwarding: %w", err)
+		}
+	}
+
+	if n.EnableIPv6 {
+		// Disable IPv6 DAD. We don't need this because this is a point to point link.
+		if _, err := sysctl.Sysctl(fmt.Sprintf("net/ipv6/conf/%s/accept_dad", hostVethLink.Attrs().Name), "0"); err != nil {
+			return fmt.Errorf("failed to disable DAD: %w", err)
+		}
+
+		// Enable IPv6. This triggers the link-local address generation.
+		if _, err := sysctl.Sysctl(fmt.Sprintf("net/ipv6/conf/%s/disable_ipv6", hostVethLink.Attrs().Name), "0"); err != nil {
+			return fmt.Errorf("failed to enable ipv6: %w", err)
+		}
+
+		// Setup proxy-ndp to the host interface
+		if _, err := sysctl.Sysctl(fmt.Sprintf("net/ipv6/conf/%s/proxy_ndp", hostVethLink.Attrs().Name), "1"); err != nil {
+			return fmt.Errorf("failed to set proxy_ndp: %w", err)
+		}
+
+		// Enable IPv6 forwarding
+		if _, err := sysctl.Sysctl(fmt.Sprintf("net/ipv6/conf/%s/forwarding", hostVethLink.Attrs().Name), "1"); err != nil {
+			return fmt.Errorf("failed to set forwarding: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func setContainerVethSysctls(n *NetConf, containerVethLink netlink.Link) error {
+	if n.EnableIPv6 {
+		// Disable IPv6 DAD. We don't need this because this is a point to point link.
+		if _, err := sysctl.Sysctl(fmt.Sprintf("net/ipv6/conf/%s/accept_dad", containerVethLink.Attrs().Name), "0"); err != nil {
+			return fmt.Errorf("failed to disable DAD: %w", err)
+		}
+
+		// Enable IPv6. This triggers the link-local address generation
+		if _, err := sysctl.Sysctl(fmt.Sprintf("net/ipv6/conf/%s/disable_ipv6", containerVethLink.Attrs().Name), "0"); err != nil {
+			return fmt.Errorf("failed to enable ipv6 for veth: %w", err)
+		}
+	}
+	return nil
+}
+
+func setDummyGatewayAddressV4(n *NetConf, hostVethLink netlink.Link) error {
+	// Assign link-scoped dummy gateway address to the host veth. We need
+	// to do this because proxy_arp only replies when the target address is
+	// reachable. Since we have a blackhole default route in the VRF table,
+	// there's no guarantee that the dummy gateway address is reachable.
+	//
+	// Note that for IPv6, we don't need to do this because kernel assigns
+	// the link-local address automatically.
+	return netlink.AddrReplace(hostVethLink, &netlink.Addr{
+		IPNet: &net.IPNet{
+			IP:   n.dummyGatewayAddressV4,
+			Mask: net.CIDRMask(32, 32),
+		},
+		Scope: int(netlink.SCOPE_LINK),
+	})
+}
+
+func createVeth(n *NetConf, netns ns.NetNS, vrf *netlink.Vrf, ifName string) (*current.Interface, *current.Interface, error) {
 	contIface := &current.Interface{}
 	hostIface := &current.Interface{}
 
@@ -298,10 +352,35 @@ func setupVeth(netns ns.NetNS, vrf *netlink.Vrf, ifName string) (*current.Interf
 		if err != nil {
 			return err
 		}
+
+		containerVethLink, err := netlink.LinkByName(containerVeth.Name)
+		if err != nil {
+			return fmt.Errorf("failed to get container veth: %w", err)
+		}
+
+		// We need to enable the container-side veth to get IPv6 link local address
+		if err := netlink.LinkSetUp(containerVethLink); err != nil {
+			return fmt.Errorf("failed to enable container veth: %w", err)
+		}
+
+		// Set sysctls for container veth
+		if err := setContainerVethSysctls(n, containerVethLink); err != nil {
+			return fmt.Errorf("failed to set container veth sysctls: %w", err)
+		}
+
+		// Wait for IPv6 link-local address to be settled
+		if n.EnableIPv6 {
+			if err := ip.SettleAddresses(containerVeth.Name, 10); err != nil {
+				return fmt.Errorf("host veth's IPv6 link-local address didn't appear: %w", err)
+			}
+		}
+
+		// Fill the CNI result
 		contIface.Name = containerVeth.Name
 		contIface.Mac = containerVeth.HardwareAddr.String()
 		contIface.Sandbox = netns.Path()
 		hostIface.Name = hostVeth.Name
+
 		return nil
 	})
 	if err != nil {
@@ -320,6 +399,25 @@ func setupVeth(netns ns.NetNS, vrf *netlink.Vrf, ifName string) (*current.Interf
 		return nil, nil, fmt.Errorf("failed to connect %q to VRF %v: %v", hostVethLink.Attrs().Name, vrf.Attrs().Name, err)
 	}
 
+	// Set sysctls for host veth
+	if err := setHostVethSysctls(n, hostVethLink); err != nil {
+		return nil, nil, fmt.Errorf("failed to set host veth sysctls: %w", err)
+	}
+
+	// Set dummy gateway address for IPv4
+	if n.EnableIPv4 {
+		if err := setDummyGatewayAddressV4(n, hostVethLink); err != nil {
+			return nil, nil, fmt.Errorf("failed to assign IPv4 dummy gateway address: %w", err)
+		}
+	}
+
+	// Wait for IPv6 link-local address to be settled
+	if n.EnableIPv6 {
+		if err := ip.SettleAddresses(hostIface.Name, 10); err != nil {
+			return nil, nil, fmt.Errorf("host veth's IPv6 link-local address didn't appear: %w", err)
+		}
+	}
+
 	return hostIface, contIface, nil
 }
 
@@ -327,52 +425,6 @@ func ensureContainerRoutes(n *NetConf, vrf *netlink.Vrf, hostInterface *current.
 	hostLink, err := netlink.LinkByName(hostInterface.Name)
 	if err != nil {
 		return err
-	}
-
-	hostVeth, ok := hostLink.(*netlink.Veth)
-	if !ok {
-		return fmt.Errorf("failed to convert link to veth")
-	}
-
-	if n.EnableIPv4 {
-		// setup proxy-arp to the host interface
-		if _, err = sysctl.Sysctl(fmt.Sprintf("net/ipv4/conf/%s/proxy_arp", hostVeth.Name), "1"); err != nil {
-			return fmt.Errorf("failed to set proxy_arp: %w", err)
-		}
-
-		// Enable IPv4 forwarding
-		if _, err = sysctl.Sysctl(fmt.Sprintf("net/ipv4/conf/%s/forwarding", hostVeth.Name), "1"); err != nil {
-			return fmt.Errorf("failed to set forwarding: %w", err)
-		}
-	}
-
-	if n.EnableIPv6 {
-		// Enable IPv6
-		if _, err = sysctl.Sysctl(fmt.Sprintf("net/ipv6/conf/%s/disable_ipv6", hostVeth.Name), "0"); err != nil {
-			return fmt.Errorf("failed to enable ipv6: %w", err)
-		}
-
-		// setup proxy-ndp to the host interface
-		if _, err = sysctl.Sysctl(fmt.Sprintf("net/ipv6/conf/%s/proxy_ndp", hostVeth.Name), "1"); err != nil {
-			return fmt.Errorf("failed to set proxy_ndp: %w", err)
-		}
-
-		// Enable IPv6 forwarding
-		if _, err = sysctl.Sysctl(fmt.Sprintf("net/ipv6/conf/%s/forwarding", hostVeth.Name), "1"); err != nil {
-			return fmt.Errorf("failed to set forwarding: %w", err)
-		}
-
-		// For IPv6, we need to setup the neighbor entry for proxying
-		if err := netlink.NeighAdd(&netlink.Neigh{
-			LinkIndex:   hostVeth.Index,
-			Family:      netlink.FAMILY_V6,
-			State:       netlink.NUD_PERMANENT,
-			IP:          n.dummyGatewayAddressV6,
-			Flags:       netlink.NTF_PROXY,
-			MasterIndex: vrf.Index,
-		}); err != nil {
-			return fmt.Errorf("failed to set proxy-ndp neighbor entry: %w", err)
-		}
 	}
 
 	// Configure direct routes to the addresses provided by the IPAM plugin
@@ -383,7 +435,7 @@ func ensureContainerRoutes(n *NetConf, vrf *netlink.Vrf, hostInterface *current.
 		case ip.Address.IP.To4() != nil && n.EnableIPv4:
 			dst.Mask = net.CIDRMask(32, 32)
 			if err := netlink.RouteAdd(&netlink.Route{
-				LinkIndex: hostVeth.Index,
+				LinkIndex: hostLink.Attrs().Index,
 				Dst:       &dst,
 				Table:     int(vrf.Table),
 				Scope:     netlink.SCOPE_LINK,
@@ -393,7 +445,7 @@ func ensureContainerRoutes(n *NetConf, vrf *netlink.Vrf, hostInterface *current.
 		case ip.Address.IP.To4() == nil && n.EnableIPv6:
 			dst.Mask = net.CIDRMask(128, 128)
 			if err := netlink.RouteAdd(&netlink.Route{
-				LinkIndex: hostVeth.Index,
+				LinkIndex: hostLink.Attrs().Index,
 				Dst:       &dst,
 				Table:     int(vrf.Table),
 				Flags:     int(netlink.FLAG_ONLINK),
@@ -409,7 +461,42 @@ func ensureContainerRoutes(n *NetConf, vrf *netlink.Vrf, hostInterface *current.
 	return nil
 }
 
-func ensureHostRoutes(n *NetConf, netns ns.NetNS, containerInterface *current.Interface, result *current.Result) error {
+func getDummyGatewayAddresses(n *NetConf, hostInterface *current.Interface) (net.IP, net.IP, error) {
+	hostVethLink, err := netlink.LinkByName(hostInterface.Name)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var v4Gw, v6Gw net.IP
+
+	if n.EnableIPv4 {
+		v4Gw = n.dummyGatewayAddressV4
+	}
+
+	if n.EnableIPv6 {
+		addrs, err := netlink.AddrList(hostVethLink, netlink.FAMILY_V6)
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to get IPv6 gateway address: %w", err)
+		}
+		if len(addrs) != 1 {
+			return nil, nil, fmt.Errorf("unexpected number of IPv6 addresses on the host veth: %v", addrs)
+		}
+		// We should have only one address (link local address) on the host veth at this point
+		v6Gw = addrs[0].IP
+	}
+
+	return v4Gw, v6Gw, nil
+}
+
+func ensureHostRoutes(n *NetConf, netns ns.NetNS, hostInterface, containerInterface *current.Interface, result *current.Result) error {
+	// Get IPv4 and IPv6 dummy gateway addresses. For IPv4, it would be a
+	// configured one. For IPv6, it's a link local address of the host
+	// veth.
+	v4Gw, v6Gw, err := getDummyGatewayAddresses(n, hostInterface)
+	if err != nil {
+		return err
+	}
+
 	// IP address should be assigned to the container interface.
 	// Fill the Interface field. We also don't provide any L2
 	// reachability among containers. Rewrite all addresses to have
@@ -430,9 +517,9 @@ func ensureHostRoutes(n *NetConf, netns ns.NetNS, containerInterface *current.In
 	for _, route := range result.Routes {
 		switch {
 		case route.Dst.IP.To4() != nil && n.EnableIPv4:
-			route.GW = n.dummyGatewayAddressV4
+			route.GW = v4Gw
 		case route.Dst.IP.To4() == nil && n.EnableIPv6:
-			route.GW = n.dummyGatewayAddressV6
+			route.GW = v6Gw
 		default:
 			return fmt.Errorf("route %s doesn't have a suitable gateway address", route.Dst.String())
 		}
@@ -446,18 +533,8 @@ func ensureHostRoutes(n *NetConf, netns ns.NetNS, containerInterface *current.In
 	if n.EnableIPv4 {
 		rt := &types.Route{
 			Dst: net.IPNet{
-				IP:   n.dummyGatewayAddressV4,
+				IP:   v4Gw,
 				Mask: net.CIDRMask(32, 32),
-			},
-			Scope: current.Int(int(netlink.SCOPE_LINK)),
-		}
-		dummyGatewayRoutes = append(dummyGatewayRoutes, rt)
-	}
-	if n.EnableIPv6 {
-		rt := &types.Route{
-			Dst: net.IPNet{
-				IP:   n.dummyGatewayAddressV6,
-				Mask: net.CIDRMask(128, 128),
 			},
 			Scope: current.Int(int(netlink.SCOPE_LINK)),
 		}
@@ -490,7 +567,7 @@ func cmdAdd(args *skel.CmdArgs) error {
 	}
 	defer netns.Close()
 
-	hostInterface, containerInterface, err := setupVeth(netns, vrf, args.IfName)
+	hostInterface, containerInterface, err := createVeth(n, netns, vrf, args.IfName)
 	if err != nil {
 		return err
 	}
@@ -532,7 +609,7 @@ func cmdAdd(args *skel.CmdArgs) error {
 	}
 
 	// Make container -> host connectivity
-	if err = ensureHostRoutes(n, netns, containerInterface, result); err != nil {
+	if err = ensureHostRoutes(n, netns, hostInterface, containerInterface, result); err != nil {
 		return fmt.Errorf("failed to setup container to host routes: %w", err)
 	}
 

plugins/hostvrf/hostvrf_test.go
@@ -67,7 +67,6 @@ func TestLoadNetConf(t *testing.T) {
 				EnableIPv4:            true,
 				EnableIPv6:            true,
 				DummyGatewayAddressV4: "169.254.0.1",
-				DummyGatewayAddressV6: "fd00::1",
 			}),
 			expectError: false,
 		},
@@ -107,10 +106,9 @@ func TestLoadNetConf(t *testing.T) {
 						Type: "foo",
 					},
 				},
-				VRFName:               "vrf0",
-				VRFTable:              100,
-				EnableIPv6:            true,
-				DummyGatewayAddressV6: "fd00::1",
+				VRFName:    "vrf0",
+				VRFTable:   100,
+				EnableIPv6: true,
 			}),
 			expectError: false,
 		},
@@ -442,6 +440,17 @@ func validateAndNormalizeRandomFields(t *testing.T, result *current.Result) {
 		// Normalize the MAC address for later comparison
 		iface.Mac = "00:00:00:00:00:00"
 	}
+
+	normalizedGw := net.ParseIP("fe80::1")
+	require.NotNil(t, normalizedGw)
+
+	for _, route := range result.Routes {
+		// If the gateway address is IPv6 link-local address, it's
+		// random. Normalize it.
+		if route.GW.To4() == nil && route.GW.IsLinkLocalUnicast() {
+			route.GW = normalizedGw
+		}
+	}
 }
 
 func TestCNIAddDel(t *testing.T) {
@@ -599,14 +608,6 @@ func TestCNIAddDel(t *testing.T) {
 							v, err = sysctl.Sysctl("net/ipv6/conf/" + hostVeth.Name + "/forwarding")
 							require.NoError(t, err)
 							require.Equal(t, "1", v, "IPv6 forwarding is not enabled on the host side veth")
-
-							neighbors, err := netlink.NeighProxyList(hostVeth.Index, netlink.FAMILY_V6)
-							require.NoError(t, err)
-							require.Len(t, neighbors, 1, "Proxy NDP neighbor entry is missing")
-
-							expected := net.ParseIP(netConf.DummyGatewayAddressV6)
-							require.NotNil(t, expected)
-							require.True(t, neighbors[0].IP.Equal(expected), "Proxy NDP is set for the wrong IP: %s", neighbors[0].IP.String())
 						}
 						return nil
 					})
@@ -738,48 +739,36 @@ func TestCNIAddDel(t *testing.T) {
 
 				t.Run("Container routes are instantiated", func(t *testing.T) {
 					err = c.ExecFuncInTestingNS(ctx, func(_ ns.NetNS) error {
-						var v4NH, v6NH net.IP
+						var v4NH net.IP
 						if netConf.EnableIPv4 {
 							v4NH = net.ParseIP(netConf.DummyGatewayAddressV4)
 						}
-						if netConf.EnableIPv6 {
-							v6NH = net.ParseIP(netConf.DummyGatewayAddressV6)
-						}
 
 						for _, route := range addResult.Routes {
-							if route.Dst.IP.Equal(v4NH) || route.Dst.IP.Equal(v6NH) {
+							if route.Dst.IP.Equal(v4NH) {
 								continue
 							}
 
-							var expectedNexthop net.IP
 							if route.Dst.IP.To4() != nil {
-								expectedNexthop = v4NH
+								rt, ok := containerRoutes[route.Dst.String()]
+								require.True(t, ok, "Route %q is not instantiated", route.Dst)
+								require.Equal(t, v4NH.String(), rt.Gw.String(), "Nexthop for the IPv4 route is wrong")
 							} else {
-								expectedNexthop = v6NH
+								require.True(t, route.GW.IsLinkLocalUnicast(), "Nexthop for the IPv6 route is not link local")
 							}
-
-							rt, ok := containerRoutes[route.Dst.String()]
-							require.True(t, ok, "Route %q is not instantiated", route.Dst)
-							require.Equal(t, expectedNexthop.String(), rt.Gw.String(), "Nexthop for the route is wrong")
 						}
 
 						return nil
 					})
 				})
 
-				t.Run("Route to the dummy gateway addresses are instantiated", func(t *testing.T) {
+				t.Run("Route to the IPv4 dummy gateway address is instantiated", func(t *testing.T) {
 					err = c.ExecFuncInTestingNS(ctx, func(_ ns.NetNS) error {
 						if netConf.EnableIPv4 {
 							rt, ok := containerRoutes[netConf.DummyGatewayAddressV4+"/32"]
 							require.True(t, ok, "IPv4 route to the dummy gateway address is missing")
 							require.Equal(t, containerVeth.Index, rt.LinkIndex, "IPv4 route to the dummy gateway address is not bounded to the interface")
 							require.Nil(t, rt.Gw, "IPv4 route to the dummy gateway address must not have a gateway")
-						}
-						if netConf.EnableIPv6 {
-							rt, ok := containerRoutes[netConf.DummyGatewayAddressV6+"/128"]
-							require.True(t, ok, "IPv6 route to the dummy gateway address is missing")
-							require.Equal(t, containerVeth.Index, rt.LinkIndex, "IPv6 route to the dummy gateway address is not bounded to the interface")
-							require.Nil(t, rt.Gw, "IPv6 route to the dummy gateway address must not have a gateway")
 						}
 						return nil
 					})